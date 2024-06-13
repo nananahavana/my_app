db3e758e2eacbf97ca9666588b3e5db09e18fd7b
nananahavana <167619957+nananahavana@users.noreply.github.com>
2024-06-13 10:41:11 +0000

    Update Latest SC

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-data = {full_name: 'Budi Gunawan', email: 'budigunawan@gmail.com', password: '1234abc'}
+data = {full_name: 'Angella Ananta', email: 'angellaab59@gmail.com', password: '123'}
 user = User.create(data)
 
 user = User.first
